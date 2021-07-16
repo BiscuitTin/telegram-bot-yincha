6a8e07f983da30fe458a8c61cef406d181893435
Garfield Lee <openpgp@550.moe>
2021-07-17 01:09:24 +0800

    feat: impl send voice message

--- lib/src/handler.rs
+++ lib/src/handler.rs
@@ -18,6 +18,7 @@ use crate::{
     media::{get_random_voice, get_voice_dir},
     settings::{Settings, Subscribe},
 };
+use chrono::Timelike;
 use teloxide::{
     prelude::{AutoSend, UpdateWithCx},
     requests::{Requester, ResponseResult},
@@ -27,27 +28,42 @@ use teloxide::{
 
 pub async fn message_handler(msg: UpdateWithCx<AutoSend<Bot>, Message>) -> ResponseResult<()> {
     let chat = &msg.update.chat;
-    let text = msg.update.text().unwrap();
-    let from = msg.update.from().unwrap();
-    // let is_admin = check_sender_is_admin(&msg.requester, chat.id).await; // TODO: Check sender is admin.
+    let t_now = chrono::Utc::now();
+    let t_hour = t_now.hour() + 8; // UTC+8
+    let t_minute = t_now.minute();
+    let t_second = t_now.second();
+    if msg.update.id == 0 {
+        log::trace!("Current time is: {}:{}:{}.", t_hour, t_minute, t_second);
+        if t_hour == 15 && t_minute == 00 && t_second < 11 {
+            voice_handler(&msg.requester, chat.id)
+                .await
+                .expect("AppError::sendVoice");
+        }
+    } else {
+        let text = msg.update.text().unwrap();
+        let from = msg.update.from().unwrap();
+        // let is_admin = check_sender_is_admin(&msg.requester, chat.id).await; // TODO: Check sender is admin.
 
-    if chat.is_group() && text.eq("/subscribe @yinchabot") {
-        let mut set = Settings::new();
-        set.add_sub(Subscribe::new(chat.id)).save();
-        msg.reply_to::<&str>("Successful subscribe this group!")
-            .await
-            .expect("AppError::sendReply");
-        log::info!(
-            "Bot successful subscribe group id: {}, message sender: {}, user id: {}.",
-            chat.id,
-            from.first_name,
-            from.id
-        );
-    }
+        if chat.is_group() && text.eq("/subscribe @yinchabot") {
+            let mut set = Settings::new();
+            set.add_sub(Subscribe::new(chat.id)).save();
+            msg.reply_to::<&str>("Successful subscribe this group!")
+                .await
+                .expect("AppError::sendReply");
+            log::info!(
+                "Bot successful subscribe group id: {}, message sender: {}, user id: {}.",
+                chat.id,
+                from.first_name,
+                from.id
+            );
+        }
 
-    #[cfg(debug_assertions)]
-    if text.contains("/test") {
-        voice_handler(&msg.requester, chat.id).await;
+        #[cfg(debug_assertions)]
+        if text.contains("/test") {
+            voice_handler(&msg.requester, chat.id)
+                .await
+                .expect("AppError::sendVoice");
+        }
     }
 
     teloxide::respond(())
